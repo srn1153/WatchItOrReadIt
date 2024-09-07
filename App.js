f5756a27a4fceeeac34b08686da96d0c2e5e718b
srn1153 <132238442+srn1153@users.noreply.github.com>
2024-09-07 13:55:48 +1200

    Adding Appwrite and creating Signin Screen to implement it

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; 
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+//import AppwriteService from './appwrite/service';
+import SigninScreen from './src/screens/SigninScreeen'; 
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Build up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <SigninScreen /> 
+    </SafeAreaView>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+}); 
+
+export default App;
