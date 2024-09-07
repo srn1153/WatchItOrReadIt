6fb8f3d2da231c032495861f783deb7319ad55a9
srn1153 <132238442+srn1153@users.noreply.github.com>
2024-09-07 14:32:52 +1200

    Adding image onto Signin Screen

--- src/screens/SigninScreeen/SigninScreen.js
+++ src/screens/SigninScreeen/SigninScreen.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native'
+import Logo from '../../../assets/images/login.png'
 
 const SigninScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.logo} />
       <Text>Signin Screen</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start', 
+    alignItems: 'center',
+  },
+  logo: {
+    justifyContent: 'flex-start',
+    resizeMode: 'stretch',
+    height: '55%', 
+    width: '100%',
+    marginTop: -48, 
+  },
+});
 
 export default SigninScreen
