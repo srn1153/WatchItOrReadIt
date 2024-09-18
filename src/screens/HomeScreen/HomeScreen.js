d0a61bc0e16840a9780e6ed6cce5cb1e86b959e6
Kimju <rfp6097@autuni.ac.nz>
2024-09-18 20:46:59 +1200

    homescreen fix

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,39 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { AntDesign } from '@expo/vector-icons';
 import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+// import * as Font from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
 import { useNavigation } from '@react-navigation/native' 
 
 const HomeScreen = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+  // const [fontsLoaded, setFontsLoaded] = useState(false);
   
   useEffect(() => {
     async function loadFonts() {
-      try {
-        await SplashScreen.preventAutoHideAsync();
-        await Font.loadAsync({
-          'ibm-plex-mono': require('./assets/fonts/IBMPlexMono-Regular.ttf'),
-        });
-        setFontsLoaded(true);
-      } catch (e) {
-        console.error(e);
-      } finally {
-        if (fontsLoaded) {
-          SplashScreen.hideAsync();
-        }
-      }
+      // try {
+      //   await SplashScreen.preventAutoHideAsync();
+      //   await Font.loadAsync({
+      //     'ibm-plex-mono': require('./assets/fonts/IBMPlexMono-Regular.ttf'),
+      //   });
+      //   setFontsLoaded(true);
+      // } catch (e) {
+      //   console.error(e);
+      // } finally {
+      //   if (fontsLoaded) {
+      //     SplashScreen.hideAsync();
+      //   }
+      // }
     }
 
     loadFonts();
-  }, [fontsLoaded]);
+  }, []); // Removed dependency on fontsLoaded
 
-  if (!fontsLoaded) {
-    return <Text>Loading...</Text>;
-  }
+  // if (!fontsLoaded) {
+  //   return <Text>Loading...</Text>;
+  // }
 
   return (
     <View>
@@ -43,7 +44,7 @@ const HomeScreen = () => {
         >
         <AntDesign name="plus" size={18} color="white" />
       </TouchableOpacity>
-      </View>
+    </View>
   );
 }
 
@@ -58,4 +59,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default HomeScreen
+export default HomeScreen;
