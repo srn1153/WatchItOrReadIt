d2df5f70a95e2ef249a627173f067bd7b6d863e0
Kimju <rfp6097@autuni.ac.nz>
2024-09-18 17:23:56 +1200

    Attempting to link both screens together

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,26 +1,61 @@
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useNavigation } from '@react-navigation/native' 
 
-//Created for temporary use, will modify once I have merged branches with team 
-//Created to test if my navgiation code was working efficiently
 const HomeScreen = () => {
-    return (
-        <View  
-        style={styles.container}
-        >
-            <Text>
-            POTENTIAL HOME PAGE
-            </Text>
-        </View>
-      );
-    };
+  const navigation = useNavigation()
 
-const styles = StyleSheet.create ({
-    container: {
-      flex: 1,
-      marginTop: Dimensions.get('window').height * 0.5,
-      alignItems: 'center',  
-    },
-  });
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  
+  useEffect(() => {
+    async function loadFonts() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          'ibm-plex-mono': require('./assets/fonts/IBMPlexMono-Regular.ttf'),
+        });
+        setFontsLoaded(true);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        if (fontsLoaded) {
+          SplashScreen.hideAsync();
+        }
+      }
+    }
+
+    loadFonts();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.plusButton}
+        onPress={() => navigation.navigate('WriteReview')}
+        >
+        <AntDesign name="plus" size={18} color="white" />
+      </TouchableOpacity>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  plusButton: {
+    marginRight: 15,
+    backgroundColor: 'grey',
+    borderRadius: 50,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default HomeScreen
