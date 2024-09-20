e45dde51ab44c0a88a7f5ea41edb74590e8c0831
srn1153 <132238442+srn1153@users.noreply.github.com>
2024-09-20 23:50:00 +1200

    Final clean up

--- src/components/utils/navigationUtils.js
+++
@@ -1,6 +0,0 @@
-export const navigateToItemDetail = (navigation, item, searchType) => {
-    const itemWithType = { ...item, type: searchType };
-    console.log('Navigating with item:', itemWithType);
-    navigation.navigate('ItemDetail', { item: itemWithType });
-  };
-  
