0bc8712163a3668b04e355ebece3eeb7383a45b3
srn1153 <132238442+srn1153@users.noreply.github.com>
2024-10-07 17:22:40 +1300

    Completed functionality, need to change locations once branches have been merged

--- src/screens/TempUserProfileRoom.js
+++ src/screens/TempUserProfileRoom.js
@@ -1,14 +1,38 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { useAuth } from '../context/authContext'
+import { useNavigation } from '@react-navigation/native'
+
 import React from 'react'
 
 const TempUserProfileRoom = () => {
+
+  //Accessing the logout function from authContext
+  const {logout} = useAuth()
+
+  //Creating a useNavigation instance to switch screens if logout is successful
+  const navigation = useNavigation()
+
+  //Created a function to handle when the signup button is pressed
+  const onLogOutPressed = async () => {
+    console.log("Logout Button was pressed!")
+
+    //Calling the logout function from authContext 
+    const response = await logout()
+    console.log("Results", response) //Print out the results
+    
+    if(response.success){ //If logout was successful
+      navigation.navigate('Login') //Navigate to the home screen 
+    }else{//Otherwise display error
+      console.log("Logout error:", response.msg)
+    }
+  }
+
   return (
     <View>
       <Text>TempUserProfileRoom</Text>
-    <TouchableOpacity>
-    <Image style={styles.image} source={require("../../assets/images/logoutButton.png")} />
-    </TouchableOpacity>
-
+        <TouchableOpacity  onPress={onLogOutPressed}>
+          <Image style={styles.image} source={require("../../assets/images/logoutButton.png")} />
+        </TouchableOpacity>
     </View>
   )
 }
