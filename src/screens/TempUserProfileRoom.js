32839e8e16251e899b8e8e89162275216c8e6c97
srn1153 <132238442+srn1153@users.noreply.github.com>
2024-10-07 17:12:15 +1300

    Adding logout button onto temporary screen

--- src/screens/TempUserProfileRoom.js
+++ src/screens/TempUserProfileRoom.js
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const TempUserProfileRoom = () => {
   return (
     <View>
       <Text>TempUserProfileRoom</Text>
+    <TouchableOpacity>
+    <Image style={styles.image} source={require("../../assets/images/logoutButton.png")} />
+    </TouchableOpacity>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({ 
+  image: {
+    height: 25, 
+    width: 25, 
+    right: 15, 
+    position: "absolute", 
+  }
+})
 
 export default TempUserProfileRoom
