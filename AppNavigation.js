ddb08298cb9cef703957de4eaf51a7b12c9019a7
srn1153 <132238442+srn1153@users.noreply.github.com>
2024-10-16 18:19:56 +1300

    Adding back button, clean up in terms of navigation headers

--- AppNavigation.js
+++ AppNavigation.js
@@ -12,7 +12,6 @@ import ItemDetailScreen from './src/screens/ItemDetailScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import ListScreen from './src/screens/ListScreen' 
 import ListSearchScreen from './src/screens/ListSearchScreen'
-import TempUserProfileRoom from './src/screens/TempUserProfileRoom'
 
 //Creating an instance of createNativeStackNavigator, to handle screen stacking
 const Stack = createNativeStackNavigator()
@@ -45,11 +44,10 @@ const AppNavigation = () => {
                 <Stack.Navigator>
                 <Stack.Screen name="Back" component={NavigationBar} options={{ headerShown: false }} />
                 <Stack.Screen name="WriteReview" component={WriteReviewScreen} />
-                <Stack.Screen name="ItemDetail" component={ItemDetailScreen} options={{ headerShown: false }}/>
-                <Stack.Screen name="ProfileRoom" component={ProfileScreen} />
+                <Stack.Screen name="ItemDetail" component={ItemDetailScreen} options={{ headerTransparent: true, headerTitle: '', headerTintColor: 'white' }}/>
+                <Stack.Screen name="ProfileRoom" component={ProfileScreen} options={{ headerTransparent: true, headerTitle: '', headerTintColor: 'white' }}/>
                 <Stack.Screen name="List" component={ListScreen} />
                 <Stack.Screen name="ListSearchScreen" component={ListSearchScreen} />
-                <Stack.Screen name='TempUserProfileRoom' component={TempUserProfileRoom} />
                 </Stack.Navigator>
             ) : (
                 <Stack.Navigator
